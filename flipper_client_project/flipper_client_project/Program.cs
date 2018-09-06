98d35a7e70754338c565aa9378fd1b5e663b4b64
without <without@local.host>
2018-09-06 15:11:23 -0400

    Created Visual Studio Project

---
+++ flipper_client_project/flipper_client_project/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        const string SOUNDFILE = @"c:/Users/wilso/Desktop/test.wav";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SoundPlayer.play(SOUNDFILE);
+            Console.ReadKey();
+        }
+    }
+
+    internal class SoundPlayer
+    {
+        public static bool play(string fileLocation)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(@"powershell", $@"-c (New-Object Media.SoundPlayer '{fileLocation}').PlaySync();");
+                return true;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Failed to play Windows file at [" + fileLocation + "], error:\n" + err);
+                return false;
+            }
+        }
+    }
+}
