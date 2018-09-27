b06b9a876b43f7c77b84ba6bb45eeb08fd9af44e
LisaPisa12 <esarq001@fiu.edu>
2018-09-27 10:42:48 -0400

    [changes]

---
+++ test.py
@@ -0,0 +1,29 @@
+print("Hello")
+
+greeting="Hello"
+
+print(greeting)
+
+myInt=35
+myStr="Hello"
+myFloat=1.9
+
+myList=[1,2,3, list]
+
+print(type(myInt),myInt)
+
+x=9
+
+if x<6:
+    print("We won")
+else:
+    print("No, we didnt")
+
+color="blue"
+
+if color=="red":
+    print("Color is red")
+elif(color=="blue"):
+    print("color is blue")
+else:
+    print("Color is not neither red nor blue")
