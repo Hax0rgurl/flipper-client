741d375dd735b3c413e389684c4950fa0be766e5
Physika0 <npabon11@gmail.com>
2018-10-25 11:18:40 -0400

    Plots fft data in wave

--- sounddevice_analyzer.py
+++ sounddevice_analyzer.py
@@ -7,6 +7,7 @@ Matplotlib and NumPy have to be installed.
 import argparse
 import queue
 import sys
+from scipy.fftpack import fft, ifft
 
 
 def int_or_str(text):
@@ -21,28 +22,38 @@ parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument(
 	'-l', '--list-devices', action='store_true',
 	help='show list of audio devices and exit')
+
 parser.add_argument(
 	'-d', '--device', type=int_or_str,
 	help='input device (numeric ID or substring)')
+
 parser.add_argument(
 	'-w', '--window', type=float, default=200, metavar='DURATION',
 	help='visible time slot (default: %(default)s ms)')
+
 parser.add_argument(
 	'-i', '--interval', type=float, default=30,
 	help='minimum time between plot updates (default: %(default)s ms)')
+
 parser.add_argument(
 	'-b', '--blocksize', type=int, help='block size (in samples)')
+
 parser.add_argument(
 	'-r', '--samplerate', type=float, help='sampling rate of audio device')
+
 parser.add_argument(
 	'-n', '--downsample', type=int, default=10, metavar='N',
 	help='display every Nth sample (default: %(default)s)')
+
 parser.add_argument(
 	'channels', type=int, default=[1], nargs='*', metavar='CHANNEL',
 	help='input channels to plot (default: the first)')
+
 args = parser.parse_args()
+
 if any(c < 1 for c in args.channels):
 	parser.error('argument CHANNEL: must be >= 1')
+
 mapping = [c - 1 for c in args.channels]  # Channel numbers start with 1
 q = queue.Queue()
 
@@ -72,7 +83,7 @@ def update_plot(frame):
 		plotdata = np.roll(plotdata, -shift, axis=0)
 		plotdata[-shift:, :] = data
 	for column, line in enumerate(lines):
-		line.set_ydata(plotdata[:, column])
+		line.set_ydata(fft(plotdata[:, column]))
 	return lines
 
 
@@ -85,6 +96,7 @@ try:
 	if args.list_devices:
 		print(sd.query_devices())
 		parser.exit(0)
+
 	if args.samplerate is None:
 		device_info = sd.query_devices(args.device, 'input')
 		args.samplerate = device_info['default_samplerate']
