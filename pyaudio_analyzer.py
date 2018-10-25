70a2995712c61431b7eedf081196078f056e9d5f
rslay <rhern283@fiu.edu>
2018-10-25 09:08:05 -0400

    Converted indentation to tabs

--- pyaudio_analyzer.py
+++ pyaudio_analyzer.py
@@ -24,12 +24,12 @@ p = pyaudio.PyAudio()
 
 # stream object to get data from microphone
 stream = p.open(
-    format=FORMAT,
-    channels=CHANNELS,
-    rate=RATE,
-    input=True,
-    output=True,
-    frames_per_buffer=CHUNK
+	format=FORMAT,
+	channels=CHANNELS,
+	rate=RATE,
+	input=True,
+	output=True,
+	frames_per_buffer=CHUNK
 )
 
 # variable for plotting
@@ -56,31 +56,31 @@ frame_count = 0
 start_time = time.time()
 
 while True:
-    
-    # binary data
-    data = stream.read(CHUNK)  
-    
-    # convert data to integers, make np array, then offset it by 127
-    data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
-    
-    # create np array and offset by 128
-    data_np = np.array(data_int, dtype='b')[::2] + 128
-    
-    line.set_ydata(data_np)
-    
-    # update figure canvas
-    try:
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-        frame_count += 1
-        
-    except TclError:
-        
-        # calculate average frame rate
-        frame_rate = frame_count / (time.time() - start_time)
-        
-        print('stream stopped')
-        print('average frame rate = {:.0f} FPS'.format(frame_rate))
-        break
+	
+	# binary data
+	data = stream.read(CHUNK)  
+	
+	# convert data to integers, make np array, then offset it by 127
+	data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
+	
+	# create np array and offset by 128
+	data_np = np.array(data_int, dtype='b')[::2] + 128
+	
+	line.set_ydata(data_np)
+	
+	# update figure canvas
+	try:
+		fig.canvas.draw()
+		fig.canvas.flush_events()
+		frame_count += 1
+		
+	except TclError:
+		
+		# calculate average frame rate
+		frame_rate = frame_count / (time.time() - start_time)
+		
+		print('stream stopped')
+		print('average frame rate = {:.0f} FPS'.format(frame_rate))
+		break
 
 
