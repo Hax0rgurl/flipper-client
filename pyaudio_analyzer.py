f7a2ab2a75e2a7b562d95d784828c1b58b2adf01
rslay <rhern283@fiu.edu>
2018-11-03 17:02:13 -0400

    Added frequency-to-hex mapping

--- pyaudio_analyzer.py
+++ pyaudio_analyzer.py
@@ -5,18 +5,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 import time
+import flipper
 
 # constants
-CHUNK = 1024 * 2             # samples per frame
-FORMAT = pyaudio.paInt16     # audio format (bytes per sample?)
-CHANNELS = 1                 # single channel for microphone
+CHUNK = 1024 * 2  # samples per frame
+FORMAT = pyaudio.paInt16  # audio format (bytes per sample?)
+CHANNELS = 1  # single channel for microphone
 RATE = 44100  # samples per second
 GRAPH_X_MIN = 800
-PLOT_OR_NOT = True
+PLOT_OR_NOT = False
 
+BIT_INTERVAL_TIME = 0.15
+BETWEEN_BIT_DELAY = 0.15
+BETWEEN_BYTE_WAIT_TIME = 0.0
 
-OFFSET = 2
-THRESHOLD = 0.16
+OFFSET = 25
+THRESHOLD = 0.17
 
 FQ_START = 17800
 FQ_STEP = 150
@@ -38,197 +42,185 @@ FQ_D = 19750
 FQ_E = 19900
 FQ_F = 20050
 
-FREQS = [FQ_0,FQ_1,FQ_2,FQ_3,FQ_4,FQ_5,FQ_6,FQ_7,FQ_8,FQ_9,FQ_A,FQ_B,FQ_C,FQ_D,FQ_E,FQ_F]
+FREQS = [
+    FQ_0,
+    FQ_1,
+    FQ_2,
+    FQ_3,
+    FQ_4,
+    FQ_5,
+    FQ_6,
+    FQ_7,
+    FQ_8,
+    FQ_9,
+    FQ_A,
+    FQ_B,
+    FQ_C,
+    FQ_D,
+    FQ_E,
+    FQ_F,
+]
+
+
+def bits2string(b=None):
+    """Function to convert binary to string"""
+    # result = ''.join([chr(int(x, 2)) for x in b])
+    result = chr(int(b, 2))
+    return result
+
 
 # pyaudio class instance
 p = pyaudio.PyAudio()
 
 # stream object to get data from microphone
 stream = p.open(
-	format=FORMAT,
-	channels=CHANNELS,
-	rate=RATE,
-	input=True,
-	output=True,
-	frames_per_buffer=CHUNK
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    output=True,
+    frames_per_buffer=CHUNK,
 )
 
 # variable for plotting
-xf = np.linspace(0, RATE, CHUNK)[GRAPH_X_MIN:int(CHUNK/2)]     # frequencies (spectrum)
+xf = np.linspace(0, RATE, CHUNK)[GRAPH_X_MIN : int(CHUNK / 2)]  # frequencies (spectrum)
 
 if PLOT_OR_NOT:
-	# create matplotlib figure and axes
-	fig, ax2 = plt.subplots()
+    # create matplotlib figure and axes
+    fig, ax2 = plt.subplots()
 
-	# create semilogx line for spectrum
-	line_fft, = ax2.plot(xf, np.random.rand(CHUNK)[GRAPH_X_MIN:int(CHUNK/2)], '-', lw=2)
+    # create semilogx line for spectrum
+    line_fft, = ax2.plot(
+        xf, np.random.rand(CHUNK)[GRAPH_X_MIN : int(CHUNK / 2)], "-", lw=2
+    )
 
+    # format waveform axes
+    ax2.set_title("Fourier Transform of Microphone Data - PyAudio")
+    ax2.set_xlabel("Frequency")
+    ax2.set_ylabel("Prevalence of Frequency")
 
-	# format waveform axes
-	ax2.set_title('Fourier Transform of Microphone Data - PyAudio')
-	ax2.set_xlabel('Frequency')
-	ax2.set_ylabel('Prevalence of Frequency')
-
-	plt.show(block=False)
+    plt.show(block=False)
 
 timerStart = time.time()
 
+output = []
+received = ""
+
+
 def findFreq(pData):
-	global timerStart  # To allow us to use timerStart within this function's scope
-	
-	for x,y in zip(xf, pData):
-		if y >= THRESHOLD:
-			if np.abs(FQ_8 - x) <= OFFSET:
-				# print("0")
-				pass
-			elif np.abs(FQ_F - x) <= OFFSET:
-				# print("1")
-				pass
+    global timerStart  # To allow us to use timerStart within this function's scope
+    global received
+    global output
+ 
+    # recieved: 
 
-			# print(x)
+    if len(received) == 2:
+        # print(": " + bits2string(received))
+        print(": " + chr(int(received,16)))
+        timerStart = time.time()  # + BETWEEN_BIT_WAIT_TIME
 
-			if PLOT_OR_NOT:
-				ax2.annotate('(%s -> %s)' % (int(x),y), xy=(x,y), textcoords='data')
+        # output <- recieved 
+        output.append(received) # output: ["1e"]
+        received = ""
 
+    # if enough time passes and no sound plays, run output[] and empty it
+    elif np.abs(timerStart - time.time()) > 2 and len(output) > 0:
+        print()
+        # os.system()
+        pass
 
+    elif np.abs(timerStart - time.time()) >= BIT_INTERVAL_TIME:
+        for x, y in zip(xf, pData):
+            if y >= THRESHOLD:
+                timerStart = time.time()
 
-	# # Frequency 0
-	# # for i in pData[FQ_8 - OFFSET:FQ_8 + OFFSET]:
-	# for x,y in zip(xf[int(len(xf)/16)*9 - OFFSET:int(len(xf)/16)*9 + OFFSET], pData[int(len(xf)/16)*9 - OFFSET:int(len(xf)/16)*9 + OFFSET]):
-	# 	if y >= THRESHOLD:
-	# 		print("Found a freq at " + FQ_8)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
+                if np.abs(FQ_0 - x) <= OFFSET:
+                    print("0", end="", flush=True)
+                    received += "0"
+                elif np.abs(FQ_1 - x) <= OFFSET:
+                    print("1", end="", flush=True)
+                    received += "1"
+                elif np.abs(FQ_2 - x) <= OFFSET:
+                    print("2", end="", flush=True)
+                    received += "2"
+                elif np.abs(FQ_3 - x) <= OFFSET:
+                    print("3", end="", flush=True)
+                    received += "3"
+                elif np.abs(FQ_4 - x) <= OFFSET:
+                    print("4", end="", flush=True)
+                    received += "4"
+                elif np.abs(FQ_5 - x) <= OFFSET:
+                    print("5", end="", flush=True)
+                    received += "5"
+                elif np.abs(FQ_6 - x) <= OFFSET:
+                    print("6", end="", flush=True)
+                    received += "6"
+                elif np.abs(FQ_7 - x) <= OFFSET:
+                    print("7", end="", flush=True)
+                    received += "7"
+                elif np.abs(FQ_8 - x) <= OFFSET:
+                    print("8", end="", flush=True)
+                    received += "8"
+                elif np.abs(FQ_9 - x) <= OFFSET:
+                    print("9", end="", flush=True)
+                    received += "9"
+                elif np.abs(FQ_A - x) <= OFFSET:
+                    print("A", end="", flush=True)
+                    received += "A"
+                elif np.abs(FQ_B - x) <= OFFSET:
+                    print("B", end="", flush=True)
+                    received += "B"
+                elif np.abs(FQ_C - x) <= OFFSET:
+                    print("C", end="", flush=True)
+                    received += "C"
+                elif np.abs(FQ_D - x) <= OFFSET:
+                    print("D", end="", flush=True)
+                    received += "D"
+                elif np.abs(FQ_E - x) <= OFFSET:
+                    print("E", end="", flush=True)
+                    received += "E"
+                elif np.abs(FQ_F - x) <= OFFSET:
+                    print("F", end="", flush=True)
+                    received += "F"
 
-	# # Frequency 1
-	# for i in pData[FQ_1 - OFFSET:FQ_1 + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_1)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency 2
-	# for i in pData[FQ_2 - OFFSET:FQ_2 + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_2)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency 3
-	# for i in pData[FQ_3 - OFFSET:FQ_3 + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_3)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency 4
-	# for i in pData[FQ_4 - OFFSET:FQ_4 + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_4)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency 5
-	# for i in pData[FQ_5 - OFFSET:FQ_5 + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_5)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency 6
-	# for i in pData[FQ_6 - OFFSET:FQ_6 + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_6)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency 7
-	# for i in pData[FQ_7 - OFFSET:FQ_7 + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_7)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency 8
-	# for i in pData[FQ_8 - OFFSET:FQ_8 + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_8)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency 9
-	# for i in pData[FQ_9 - OFFSET:FQ_9 + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_9)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency A
-	# for i in pData[FQ_A - OFFSET:FQ_A + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_A)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency B
-	# for i in pData[FQ_B - OFFSET:FQ_B + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_B)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency C
-	# for i in pData[FQ_C - OFFSET:FQ_C + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_C)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency D
-	# for i in pData[FQ_D - OFFSET:FQ_D + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_D)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency E
-	# for i in pData[FQ_E - OFFSET:FQ_E + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_E)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
-	# # Frequency F
-	# for i in pData[FQ_F - OFFSET:FQ_F + OFFSET]:
-	# 	if i >= THRESHOLD:
-	# 		# print(i)
-	# 		print("Found a freq at " + FQ_F)
-	# 		timerStart = time.time() # Reset the timer because we found a new beep
-
+                if PLOT_OR_NOT:
+                    ax2.annotate("(%s -> %s)" % (int(x), y), xy=(x, y), textcoords="data")
+                break
 
 
 while True:
 
-	# binary data
-	data = stream.read(CHUNK, exception_on_overflow=False)
+    # binary data
+    data = stream.read(CHUNK, exception_on_overflow=False)
 
-	# convert data to integers, make np array, then offset it by 127
-	data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
+    # convert data to integers, make np array, then offset it by 127
+    data_int = struct.unpack(str(2 * CHUNK) + "B", data)
 
-	# create np array and offset by 128
-	data_np = np.array(data_int, dtype='b') + 128
-	# compute FFT and update line
-	yf = fft(data_int)
-	yf = (np.abs(yf[0:CHUNK]) / (128 * CHUNK))
+    # create np array and offset by 128
+    data_np = np.array(data_int, dtype="b") + 128
+    # compute FFT and update line
+    yf = fft(data_int)
+    yf = np.abs(yf[0:CHUNK]) / (128 * CHUNK)
 
-	findFreq(yf[GRAPH_X_MIN:int(CHUNK / 2)])
-	
-	if PLOT_OR_NOT:
-		pass
-		line_fft.set_ydata(yf[GRAPH_X_MIN:int(CHUNK/2)])
+    findFreq(yf[GRAPH_X_MIN : int(CHUNK / 2)])
 
-		try:
-			fig.canvas.draw()
-			fig.canvas.flush_events()
-			plt.pause(0.0001)
-		except Exception:
-			exit(0)
+    if PLOT_OR_NOT:
+        pass
+        line_fft.set_ydata(yf[GRAPH_X_MIN : int(CHUNK / 2)])
+
+        try:
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+            plt.pause(0.0001)
+        except Exception:
+            exit(0)
+
+
+def hexToASCII(parameter):
+    ascii = bytes.fromhex(parameter).decode("ascii")
+    test = bin(parameter)
+    leascii = ascii(test)
+    print(leascii)
+    # hex()
+    return ascii
+
